59e2b94ff032957c7a5989c1945f2da99194341e
Stefan Arentz <stefan@arentz.ca>
2017-05-07 18:29:08 -0400

    Updates

--- requestinfo.go
+++ requestinfo.go
@@ -7,6 +7,7 @@ package requestinfo
 import (
 	"encoding/json"
 	"net/http"
+	"crypto/tls"
 
 	"github.com/mholt/caddy/caddyhttp/httpserver"
 )
@@ -16,11 +17,17 @@ type RequestInfo struct {
 	Path string
 }
 
+type TLSInfo struct {
+	Version string `json:"version"`
+	CipherSuite string `json:"cipherSuite"`
+}
+
 type RequestInfoResponse struct {
-	Address  string
-	Method   string
-	Protocol string
-	Headers  map[string][]string
+	Address  string `json:"address"`
+	Method   string `json:"method"`
+	Protocol string `json:"protocol"`
+	TLS *TLSInfo `json:"tls,omitempty"`
+	Headers  map[string][]string `json:"headers"`
 }
 
 func protocol(r *http.Request) string {
@@ -31,12 +38,88 @@ func protocol(r *http.Request) string {
 	}
 }
 
+func cipherSuiteName(suite uint16) string {
+	switch suite {
+	case tls.TLS_RSA_WITH_RC4_128_SHA:
+		return "TLS_RSA_WITH_RC4_128_SHA"
+	case tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA:
+		return "TLS_RSA_WITH_3DES_EDE_CBC_SHA"
+	case tls.TLS_RSA_WITH_AES_128_CBC_SHA:
+		return "TLS_RSA_WITH_AES_128_CBC_SHA"
+	case tls.TLS_RSA_WITH_AES_256_CBC_SHA:
+		return "TLS_RSA_WITH_AES_256_CBC_SHA"
+	case tls.TLS_RSA_WITH_AES_128_CBC_SHA256:
+		return "TLS_RSA_WITH_AES_128_CBC_SHA256"
+	case tls.TLS_RSA_WITH_AES_128_GCM_SHA256:
+		return "TLS_RSA_WITH_AES_128_GCM_SHA256"
+	case tls.TLS_RSA_WITH_AES_256_GCM_SHA384:
+		return "TLS_RSA_WITH_AES_256_GCM_SHA384"
+	case tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:
+		return "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA"
+	case tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:
+		return "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"
+	case tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:
+		return "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA"
+	case tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA:
+		return "TLS_ECDHE_RSA_WITH_RC4_128_SHA"
+	case tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA:
+		return "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA"
+	case tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:
+		return "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA"
+	case tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:
+		return "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
+	case tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256:
+		return "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
+	case tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256:
+		return "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
+	case tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
+		return "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+	case tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
+		return "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+	case tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:
+		return "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+	case tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
+		return "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+	case tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305:
+		return "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
+	case tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305:
+		return "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
+	}
+	return "UNKNOWN"
+}
+
+func tlsVersionName(version uint16) string {
+	switch version {
+	case tls.VersionSSL30:
+		return "SSL 3.0"
+	case tls.VersionTLS10:
+		return "TLS 1.0"
+	case tls.VersionTLS11:
+		return "TLS 1.1"
+	case tls.VersionTLS12:
+		return "TLS 1.2"
+	}
+	return "UNKNOWN"
+}
+
+func tlsInfo(r *http.Request) *TLSInfo {
+	if r.TLS == nil {
+		return nil
+	}
+
+	return &TLSInfo{
+		Version: tlsVersionName(r.TLS.Version),
+		CipherSuite: cipherSuiteName(r.TLS.CipherSuite),
+	}
+}
+
 func (h RequestInfo) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
 	if r.URL.Path == h.Path {
 		requestInfoResponse := RequestInfoResponse{
 			Address:  r.RemoteAddr,
 			Method:   r.Method,
 			Protocol: protocol(r),
+			TLS:     tlsInfo(r),
 			Headers:  r.Header,
 		}
 		w.Header().Add("Content-Type", "application/json")
